634c4567f73b4fbc7ab87c4c9f96bbb37abdfaa0
Bryn Beaudry <babeaudry@gmail.com>
2017-05-15 10:56:32 -0700

    the top appears to be done, logo were made darker.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <div class="head-text">
     <img class="img-responsive main-logo-img" src="{{asset('images/main-logo.gif')}}" />
     <h1>Welcome to BBEAU.CA.</h1>
-    <h2>Make it work beautifully.</h2>
+    <h2>Show your work, beautifully.</h2>
     <p>Get Bryn to class up your website</a>
         <br />
         or develop tools that make your work easier. </p>
